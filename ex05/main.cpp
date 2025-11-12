c2d34ea02e9634e4586308e56f9c2ba566844b38
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-11-12 19:45:14 +0100

    [ex05] Minor changes in printing the output

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -4,9 +4,17 @@
 int main(void)
 {
 	Harl harl;
+	std::cout<< "\nIn order\n";
 	harl.complain("DEBUG");
 	harl.complain("INFO");
 	harl.complain("WARNING");
 	harl.complain("ERROR");
+	std::cout<< "\nOut of original order\n";
+	harl.complain("ERROR");
+	harl.complain("INFO");
+	harl.complain("DEBUG");
+	harl.complain("WARNING");
+	std::cout<< "--- END ---\n";
+
 	return 0;
 }
