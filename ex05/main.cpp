f45736cc8ef1a8c733cb4dfee8a65f4eda447253
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-11-12 19:39:15 +0100

    [ex05] Completed

---
+++ ex05/main.cpp
@@ -0,0 +1,12 @@
+#include "Harl.hpp"
+#include <iostream>
+
+int main(void)
+{
+	Harl harl;
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	return 0;
+}
