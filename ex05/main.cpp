9bb3ddf7de5639d366a87c3ce09666e82f1009b8
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2025-11-12 20:06:33 +0100

    [ex05] minor change

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -4,17 +4,17 @@
 int main(void)
 {
 	Harl harl;
-	std::cout<< "\nIn order\n";
+	std::cout<< "\nIn order" << std::endl;
 	harl.complain("DEBUG");
 	harl.complain("INFO");
 	harl.complain("WARNING");
 	harl.complain("ERROR");
-	std::cout<< "\nOut of original order\n";
+	std::cout << "\nOut of original order" << std::endl;
 	harl.complain("ERROR");
 	harl.complain("INFO");
 	harl.complain("DEBUG");
 	harl.complain("WARNING");
-	std::cout<< "--- END ---\n";
+	std::cout << "--- END ---\n" << std::endl;
 
 	return 0;
 }
